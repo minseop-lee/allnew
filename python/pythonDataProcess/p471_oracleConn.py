378492336f0325428d27534c634d0b6cc5b31aed
minseop-lee <lyll4192@gmail.com>
2023-05-16 10:38:30 +0900

    df

---
+++ python/pythonDataProcess/p471_oracleConn.py
@@ -0,0 +1,33 @@
+import cx_Oracle
+
+cx_Oracle.init_oracle_client(lib_dir="/Users/seop/OracleXE/instantclient_19_10")
+conn = None # 접속 객체
+cur = None # 커서 객체
+
+try:
+    # 아이디/비번@hostname:port_number/sid
+    loginfo = 'hr/1234@192.168.1.199:1521/xe'
+    conn = cx_Oracle.connect(loginfo)
+    print(type(conn))
+
+    cur = conn.cursor()
+    print(type(cur))
+
+    sql = 'select power(2,10) from dual'
+    cur.execute(sql)
+
+    for item in cur:
+        print(item)
+
+
+except Exception as err:
+    print(err)
+
+finally:
+    if cur != None:
+        cur.close()
+
+    if conn != None:
+        conn.close()
+
+print('finished')
